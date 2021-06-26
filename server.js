0583f5905a37bdc2a66c80a35c6a93c492d7214d
Saatvik07 <85619385+m-a-x-i-m-u-s@users.noreply.github.com>
2021-06-26 06:55:47 +0530

    Test

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const errorHandler = require("errorhandler");
 const mongoose = require("mongoose");
@@ -15,7 +14,6 @@ const PORT = process.env.PORT || 8080;
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
-app.use(bodyParser());
 app.use(morgan("dev"));
 const MONGODB_URI2 = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ldxgo.gcp.mongodb.net/Optimizer101?retryWrites=true&w=majority`;
 mongoose.connect(MONGODB_URI2, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
